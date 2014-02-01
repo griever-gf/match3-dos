8180d89a2a920dd1591ed53845665fbad70eca1e
Griever <radical.griever@gmail.com>
2014-02-01 20:26:34 +0400

    more proper sprites generation

--- Assets/Filler.cs
+++ Assets/Filler.cs
@@ -28,21 +28,31 @@ public class Filler : MonoBehaviour {
 			for (int j = 0; j < spritesJewels.GetLength(1); j++)
 			{
 				spritesJewels[i,j] = Instantiate(prefabJewel, tilemap.GetTilePosition(i,j)+ tilesize*0.5f, transform.rotation) as GameObject;//as tk2dSprite;
-				Debug.Log(spritesJewels[i,j].GetComponent<tk2dSprite>().spriteId);
-				//Debug.Log (spritesJewels[i,j].name);
-				//spritesJewels[i,j].Collection.Id;
-				//spritesJewels[i,j].SetSprite("Jewel_5");
+				//Debug.Log(spritesJewels[i,j].GetComponent<tk2dSprite>().spriteId);
 			}
 		int SpritesCount = spritesJewels[0,0].GetComponent<tk2dSprite>().Collection.Count;
 		System.Random random = new System.Random();
+		int NewSpriteID;
+		bool IsMatch3;
 
 		for (int i = 0; i < spritesJewels.GetLength(0); i++)
 			for (int j = 0; j < spritesJewels.GetLength(1); j++)
-				spritesJewels[i,j].GetComponent<tk2dSprite>().SetSprite(random.Next(0, SpritesCount));
-		//Debug.Log("Sprite Variants: " + sp.Collection.Count);
-		//for (int i = 0; i < spritesJewels.GetLength(0); i++)
-			//for (int j = 0; j < spritesJewels.GetLength(1); j++)
-				//j = 1;
-				//spritesJewels[i,j].SetSprite("Jewel_5");
+			{
+				do
+				{
+					NewSpriteID = random.Next(0, SpritesCount);
+					IsMatch3 = false;
+					if (i>=2) IsMatch3 			   = IsMatch3 || ((spritesJewels[i-1,j].GetComponent<tk2dSprite>().spriteId == NewSpriteID)||
+				                                        		  (spritesJewels[i-2,j].GetComponent<tk2dSprite>().spriteId == NewSpriteID));
+					if (i<SpritesCount-2) IsMatch3 = IsMatch3 || ((spritesJewels[i+1,j].GetComponent<tk2dSprite>().spriteId == NewSpriteID)||
+				                                              	  (spritesJewels[i+2,j].GetComponent<tk2dSprite>().spriteId == NewSpriteID));
+					if (j>=2) IsMatch3 			   = IsMatch3 || ((spritesJewels[i,j-1].GetComponent<tk2dSprite>().spriteId == NewSpriteID)||
+					                                        	  (spritesJewels[i,j-2].GetComponent<tk2dSprite>().spriteId == NewSpriteID));
+					if (j<SpritesCount-2) IsMatch3 = IsMatch3 || ((spritesJewels[i,j+1].GetComponent<tk2dSprite>().spriteId == NewSpriteID)||
+					                                              (spritesJewels[i,j+2].GetComponent<tk2dSprite>().spriteId == NewSpriteID));
+				}
+				while (IsMatch3);
+				spritesJewels[i,j].GetComponent<tk2dSprite>().SetSprite(NewSpriteID);
+			}
 	}
 }
